bef2e74d684063d35be64f0efe45ccca22ca8adc
d-lino <diannalinno@gmail.com>
2025-02-15 21:38:58 -0300

    missão 1, 2 e 3

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-# Run `python main.py` in the terminal
+# Missão 1: Restaurando as Regras Escolares 📝
 
-# Note: Python is lazy loaded so the first run will take a moment,
-# But after cached, subsequent loads are super fast! ⚡️
+nota = float(input("Digite a nota do aluno: "))
 
-import platform
-print(f"Hello Python v{platform.python_version()}!")
+if nota >= 6:
+    print("O aluno está aprovado")
+elif nota <= 5:
+    print("O Aluno foi reprovado!")
+else:
+    print("Nota inválida!")
+
+# Missão 2: O Sistema Eleitoral Secreto 📝 
+
+Dianna = 17
+
+if Dianna >= 16:
+    print("pode votar")
+else:
+    print("não pode votar")
+
+# Missão 3: Recuperando o Sistema de Notas 📊
+
+nota_aluno = int(input("Digite a nota do aluno: "))
+
+if nota_aluno >= 90:
+    print("parabéns você tirou A!")
+elif nota_aluno >= 80:
+    print("Muito bem, você tirou B")
+elif nota_aluno >= 70:
+    print("Bom trabalho, você tirou C")
+elif nota_aluno >= 60:
+    print("Fique atento! você tirou D")
+elif nota_aluno < 60:
+    print("Estude mais! você tirou F")
